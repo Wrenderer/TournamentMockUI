05bcc6b6f9f72122ca2f0476fe73d42c7aac3b05
Wildrender <cil.family@gmail.com>
2020-06-15 01:36:34 -0500

    Add Dashboard Component Tests
     * Add `dashboard` spec file tests
     * Adjust `app` spec file formatting slightly
    
    Addresses: #20

--- src/app/app.component.spec.ts
+++ src/app/app.component.spec.ts
@@ -5,11 +5,7 @@ import { Title } from '@angular/platform-browser';
 import { Router, NavigationEnd, Routes } from '@angular/router';
 import { Component } from '@angular/core';
 
-@Component({
-  selector: '',
-  template: '',
-  styles: ['']
-})
+@Component({selector: '', template: '', styles: ['']})
 class MockComponent {}
 
 describe('AppComponent', () => {
--- src/app/dashboard/dashboard.component.spec.ts
+++ src/app/dashboard/dashboard.component.spec.ts
@@ -2,17 +2,42 @@ import { async, ComponentFixture, TestBed } from '@angular/core/testing';
 
 import { DashboardComponent } from './dashboard.component';
 import { RouterTestingModule } from '@angular/router/testing';
+import { FakeOidcService } from '../fake-oidc.service';
+import { Router } from '@angular/router';
+import { Component } from '@angular/core';
+
+@Component({selector: '', template: '', styles: ['']})
+class MockComponent {}
 
 describe('DashboardComponent', () => {
   let component: DashboardComponent;
   let fixture: ComponentFixture<DashboardComponent>;
+  let oidc: FakeOidcService;
+
+  const oidcSpy: jasmine.SpyObj<FakeOidcService> = jasmine.createSpyObj(
+    'FakeOidcService',
+    ['authorize', 'deAuthorize', 'isAuthorized']
+  );
+
+  const mockRouter = {
+    navigate: jasmine.createSpy('navigate')
+  }
 
   beforeEach(async(() => {
     TestBed.configureTestingModule({
-      imports: [ RouterTestingModule ],
-      declarations: [ DashboardComponent ]
+      imports: [ ],
+      declarations: [ DashboardComponent ],
+      providers: [
+        { provide: FakeOidcService, useValue: oidcSpy },
+        { provide: Router, useValue: mockRouter }
+      ]
     })
-    .compileComponents();
+    .compileComponents().then(() => {
+      oidc = TestBed.inject(FakeOidcService) as jasmine.SpyObj<FakeOidcService>;
+      oidc.authorize = jasmine.createSpy('authorize').and.callFake(() => {});
+      oidc.deAuthorize = jasmine.createSpy('deAuthorize').and.callFake(() => {});
+      oidc.isAuthorized = jasmine.createSpy('isAuthorized').and.returnValue(true);
+    });
   }));
 
   beforeEach(() => {
@@ -23,5 +48,15 @@ describe('DashboardComponent', () => {
 
   it('should create', () => {
     expect(component).toBeTruthy();
+  });
+
+  describe('#logout', () => {
+    it('should end the session and send the user to the login page', () => {
+      expect(oidc.deAuthorize).not.toHaveBeenCalled();
+      expect(mockRouter.navigate).not.toHaveBeenCalled();
+      component.logout();
+      expect(oidc.deAuthorize).toHaveBeenCalled();
+      expect(mockRouter.navigate).toHaveBeenCalled();
+    });
   });
 });
